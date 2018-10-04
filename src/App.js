9446849bdc7a323059306c43e98382b50661ebaa
Libby Colbert <colbert.libby@gmail.com>
2018-10-03 21:32:58 -0400

    Add dynamic map

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react';
 import './css/App.css';
-import Map from './components/MapContainer'
+import MapContainer from './components/MapContainer';
 
 class App extends Component {
+      state = {
+        staticMap: [],
+        defaultMapProps: 
+        [
+          {center : [33.748995, -84.387982]},
+          {zoom : 10}
+        ]
+    }
+
+
+    componentDidMount() {
+        let key = `key=AIzaSyBR94Y6cJWdYrdIJ_LjSites5nBTwL9yhs`
+        let center = `center=33.748995,-84.387982`
+        let size = `size=400x400`
+        let zoom = `zoom=10`
+        const parameters = [
+            `${size}`,
+            `${center}`,
+            `${zoom}`,
+            `${key}`,
+        ].join("&")
+        let url = `https://maps.googleapis.com/maps/api/staticmap?${parameters}`
+        fetch(url)
+            .then(response => {
+                if (response.ok) {
+                    return response.blob()
+                }
+            })
+            .then(data => {
+                this.setState({ staticMap: URL.createObjectURL(data) });
+                return(this.state.staticMap)
+            })
+            .catch(err => {
+                console.log(`didn't fetch map: err ${err}`)
+            })
+    }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header role="banner" className="App-header">
           <h1>
             Party On!
           </h1>
-          <Map/>
         </header>
+          <MapContainer copyOfMapAtl={this.state.staticMap} />
       </div>
     );
   }
--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,52 +1,44 @@
 import React, { Component } from 'react';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import '../css/App.css'
 // import PropTypes from 'propTypes'
 
 
 class MapContainer extends Component {
 
     render() {
-        const style = {
-            height: '100%',
-            // width: '50%'
-        }
-
         return (
-            // <div style={style}>
-            <div>
-
-                <div className="drop-down-menu">
-                    <select>
-                        <option value="Select a">Select a</option>
-                        <option value="2">Buckhead</option>
-                        <option value="3">Downtown</option>
-                        <option value="4">Midtown</option>
-                    </select>
-                </div>
+            <main role="main">
                 <div>
-                    <img src="https://maps.googleapis.com/maps/api/staticmap?zoom=10&size=300x300&center=33.748995,-84.387982&key=AIzaSyBR94Y6cJWdYrdIJ_LjSites5nBTwL9yhs&markers=color:red%7Clabel:A%7Clocation:33.748995,-84.387982" alt="map of Atlanta" />
-                    {/* <img src={this.url} alt="map" /> */}
-                    {/* <Map
-                        google={this.props.google}
-                        style={style}
-                        initialCenter={{
-                            lat: 33.748995,
-                            lng: -84.387982
-                        }}
-                        zoom={10}
-                    />
-                        <Marker
-                            title={'The marker`s title will appear as a tooltip.'}
-                            name={'SOMA'}
-                            // position={{ lat: 37.74899, lng: -84.387982 }}
-                        /> */}
+                    <div className="drop-down-menu">
+                        <select>
+                            <option value="Select a">Select a</option>
+                            <option value="2">Buckhead</option>
+                            <option value="3">Downtown</option>
+                            <option value="4">Midtown</option>
+                        </select>
+                    </div>
+                    <div id="map" role="application">
+                        <img src={this.props.copyOfMapAtl} alt="map" />
+                        {/* <Map
+                            google={this.props.google}
+                            initialCenter={{
+                                lat: 33.748995,
+                                lng: -84.387982
+                            }}
+                            zoom={10} >
+                            <Marker
+                                name={'test'}
+                                position={{ lat: 33.748995, lng: -84.387982 }} />
+                        </Map> */}
+                    </div>
                 </div>
-            </div>
+            </main >
         )
     }
 }
 
-// export default MapContainer
+
 export default GoogleApiWrapper({
     apiKey: 'AIzaSyBR94Y6cJWdYrdIJ_LjSites5nBTwL9yhs'
 })(MapContainer)
