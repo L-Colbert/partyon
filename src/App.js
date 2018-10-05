c018b88598a4d634386143099d6af09a30bc7ed0
Libby Colbert <colbert.libby@gmail.com>
2018-10-04 21:10:11 -0400

    Change function organization

--- src/App.js
+++ src/App.js
@@ -9,23 +9,38 @@ class App extends Component {
       [
         { center: [33.748995, -84.387982] },
         { zoom: 10 }],
-    venues: []
+    nightSpots: []
   }
-
-
-  componentDidMount() {
+  
+  loadMap = () => {
     let key = `key=AIzaSyBR94Y6cJWdYrdIJ_LjSites5nBTwL9yhs`
     let center = `center=33.748995,-84.387982`
     let size = `size=300x300`
     let zoom = `zoom=10`
-
+    
     const mapParams = [
       `${size}`,
       `${center}`,
       `${zoom}`,
       `${key}`,
     ].join("&")
+    let mapUrl = `https://maps.googleapis.com/maps/api/staticmap?${mapParams}`
 
+    fetch(mapUrl)
+    .then(response => {
+      if (response.ok) {
+        return response.blob()
+      }
+    }).then(data => {
+      this.setState({ staticMap: URL.createObjectURL(data) });
+      return (this.state.staticMap)
+    }).catch(err => {
+      console.log(`didn't fetch map: err ${err}`)
+    })
+
+  }
+
+  getNightSpots = () => {
     // GET https://api.foursquare.com/v2/venues/search
     // GET https://api.foursquare.com/v2/venues/explore
 
@@ -39,22 +54,10 @@ class App extends Component {
       `client_id=3ZV20H0X5WOSYXQQ2FVI0NHCNGPYLTHUZQLRE1EVOTRGHYKP`,
       `client_secret=3AOFNXLIEMMCFLR3VSXRALYVCWUYFT4SEVXYUTSKKD3WJWXV`,
       `v=20181003`
-].join('&')
+    ].join('&')
 
-    let mapUrl = `https://maps.googleapis.com/maps/api/staticmap?${mapParams}`
     let fourSqUrl = `https://api.foursquare.com/v2/venues/explore?${fourSqParams}`
-    
-    fetch(mapUrl)
-      .then(response => {
-        if (response.ok) {
-          return response.blob()
-        }
-      }).then(data => {
-        this.setState({ staticMap: URL.createObjectURL(data) });
-        return (this.state.staticMap)
-      }).catch(err => {
-        console.log(`didn't fetch map: err ${err}`)
-      })
+
 
     fetch(fourSqUrl)
       .then(response => {
@@ -64,14 +67,20 @@ class App extends Component {
       }).then(data => {
         console.log(data.response.groups[0].items)
         let venueIds = data.response.groups[0].items.map(dataItem => {
-         return {"venueId" : dataItem.venue.id, "lat": dataItem.venue.location.lat, "long": dataItem.venue.location.lng }
-        }) 
-        this.setState( {venues : venueIds } )
+          return { "venueId": dataItem.venue.id, "lat": dataItem.venue.location.lat, "long": dataItem.venue.location.lng }
+        })
+        this.setState({ nightSpots: venueIds })
         return venueIds
-      }).then(() => {console.log(this.state.venues)})
+      }).then(() => { console.log(this.state.nightSpots) })
       .catch(error => {
         console.log(`this is your error ${error}`)
       })
+
+  }
+
+  componentDidMount() {
+    this.loadMap()
+    this.getNightSpots()
   }
 
   render() {
@@ -83,7 +92,7 @@ class App extends Component {
           </h1>
         </header>
         <MapContainer copyOfMapAtl={this.state.staticMap}
-        venues={this.state.venues}
+          nightSpots={this.state.nightSpots}
         />
       </div>
     );
